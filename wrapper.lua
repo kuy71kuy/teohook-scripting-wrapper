4cded05c2850dd69f2f25227aa6b0149bb64578c
ManualMap <60797172+ManualMap@users.noreply.github.com>
2021-08-17 18:10:58 +0300

    add face side

--- wrapper.lua
+++ wrapper.lua
@@ -6,3 +6,25 @@ end
 function joinWorld(world)
 	sendPacket(false, "action|join_request\nname|" .. world, 3);
 end
+
+function faceSide(side)
+	if side == "left" then
+		packet = {}
+		packet.type = 0
+		packet.pos_x = getLocal().pos.x
+		packet.pos_y = getLocal().pos.y
+		packet.flags = 48
+		sendPacketRaw(false, packet)
+		return
+	end
+	if side == "right" then
+		packet = {}
+		packet.type = 0
+		packet.pos_x = getLocal().pos.x
+		packet.pos_y = getLocal().pos.y
+		packet.flags = 32
+		sendPacketRaw(false, packet)
+		return;
+	end
+	log("Invalid side chosen (" .. side .. ")")
+end
